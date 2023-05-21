96456537b02b1db58eec6376ada8b39db66f03aa
Amélie <ameliedelloro@gmail.com>
2023-05-21 17:31:36 +0200

    CreateNewRenText script added

---
+++ Assets/CreateNewRenText.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CreateNewRenText : MonoBehaviour
+{
+
+    // public RenderTexture renderTexture;
+    public int textureWidth = 512;
+    public int textureHeight = 512;
+    public RenderTextureFormat textureFormat = RenderTextureFormat.Default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create the RenderTexture
+        RenderTexture renderTexture = new RenderTexture(textureWidth, textureHeight, 0, textureFormat);
+
+        // Assign the RenderTexture to the "SDF Texture" component if it exists
+        SDFTexture sdfTexture = GetComponent<SDFTexture>();
+        Debug.Log(sdfTexture);
+        Debug.Log(sdfTexture.sdf);
+        if (sdfTexture != null)
+        {
+            sdfTexture.sdf = renderTexture;
+            Debug.Log(sdfTexture.sdf);
+        }
+    }
+
+}
