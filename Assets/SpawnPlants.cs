7a33a7411f929657431080e9342e6283b1656a93
Am.D <ameliedelloro@gmail.com>
2023-06-13 17:39:10 +0200

    spawn plants in area around spirit

--- Assets/SpawnPlants.cs
+++ Assets/SpawnPlants.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SpawnPlants : MonoBehaviour
 {
     public GameObject[] prefabs;
-    public float spawnDelay = 0.3f;
+    public float spawnDelay = 0.2f;
     public float despawnDelay = 4f;
-    public int maxSpawnCount = 10;
+    public int maxSpawnCount = 15;
     public int spawnY = 0;
+    public float spawnRadius = 1f;
     public float maxY = 2;
 
     private float lastSpawnTime;
@@ -21,8 +22,9 @@ public class SpawnPlants : MonoBehaviour
             if (Time.time > lastSpawnTime + spawnDelay && spawnedObjects.Count < maxSpawnCount && transform.position.y >= spawnY && transform.position.y <= maxY)
             {
                 int randomIndex = Random.Range(0, prefabs.Length);
+                Vector2 randomOffset = Random.insideUnitCircle * spawnRadius;
                 GameObject prefab = prefabs[randomIndex];
-                GameObject newObject = Instantiate(prefab, new Vector3(transform.position.x, 0f, transform.position.z), Quaternion.identity);
+                GameObject newObject = Instantiate(prefab, new Vector3(transform.position.x + randomOffset.x, 0f, transform.position.z + randomOffset.y), Quaternion.identity);
                 spawnedObjects.Add(newObject);
                 lastSpawnTime = Time.time;
 
